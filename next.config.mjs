dd080bf2fa82178256102edb284cd8664f039e83
Ibragim <97885427+Heilonng23@users.noreply.github.com>
2024-11-01 11:30:56 +0100

    new

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["cdn.dribbble.com"], // Allow images from Dribbble CDN
+  },
+};
+
+export default nextConfig;
